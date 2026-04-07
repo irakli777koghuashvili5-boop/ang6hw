3a03364a6d7d04d1fcb902766d5bff9d125c186c
Default <irakli777koghuashvili5@gmail.com>
2026-04-07 11:12:19 +0400

    hw 6-2

---
+++ src/app/error/error.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-error',
+  imports: [RouterLink],
+  templateUrl: './error.html',
+  styleUrl: './error.scss',
+})
+export class Error {
+  constructor(private x: Router){
+    console.log(this.x);
+  }
+  user =`user`
+  id = 1;
+
+  goToHome(){
+    this.x.navigateByUrl(`/home?id=${this.id}`);
+  }
+
+}
